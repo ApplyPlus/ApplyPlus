052027bb624193c8332bd4e969eb365d610afc01
Vikram N. Subramanian <vikram.subramanian6@gmail.com>
2020-07-09 12:57:09 -0400

    Setup a basic main file than can run git apply and take in args

--- scripts/apply.py
+++ scripts/apply.py
@@ -7,18 +7,39 @@ import check_file_exists_elsewhere as fileCheck
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--reverse", help="Apply a patch in reverse files and/or to the index")
+        "--reverse", help="Apply a patch in reverse files and/or to the index.", action='store_true')
+
+    parser.add_argument(
+        'pathToPatch', help="Path to the patch that needs to be applied.")
 
     args = parser.parse_args()
     return args
 
 
+def apply_reverse(pathToPatch):
+    print ("Apply in reverse called")
+
+
+
+def apply(pathToPatch):
+    print("apply called")
+    patch_file = parse.PatchFile(pathToPatch)
+    patch_file.runPatch()
+    if(patch_file.runSuccess == True):
+        print("Successfully applied")
+        return 0
+    else:
+        print("Patch failed with error:")
+        print(patch_file.runResult)
+        return 1
+
+
 def main():
-    patch_file = parse.PatchFile("patch.patch")
-    patch_file.getPatch()
-    fileCheck.checkFileExistsElsewhere(patch_file.patches[0])
+    if (args.reverse):
+        apply_reverse(args.pathToPatch)
+    else:
+        apply(args.pathToPatch)
 
 if __name__ == "__main__":
     args = get_args()
-    print(args.apply)
     main()
