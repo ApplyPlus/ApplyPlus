20e065c21dbee828e682b1422751935b957eea64
YinuoWang <yn6wang@edu.uwaterloo.ca>
2020-07-14 22:12:12 -0400

    Switched to pygments lexer. Now supports lexing for C, C++, C#, Java

--- scripts/test_match.py
+++ scripts/test_match.py
@@ -2,7 +2,7 @@ import diff_match_patch as dmp_module
 import patchParser as parse 
 from enum import Enum
 import Levenshtein
-from pycparser import c_lexer as clex
+from pygments.lexers import CLexer, CppLexer, CSharpLexer, JavaLexer, get_lexer_for_filename
 
 dmp = dmp_module.diff_match_patch()
 dmp.Match_Distance = 2000
@@ -15,16 +15,6 @@ does not always contain the entire patch, so our matched code is the section fro
 """
 PATCH_LENGTH_BUFFER = 10
 
-# Defining the C Lexer
-noop = lambda *args, **kwargs:None
-def raise_lex_error(error_msg, _line, _col):
-    error_msg_header = "Error occured while lexing file/patch - here is the error mesage:"
-    fix_suggestion = "Either fix the lexing error, or stop scanning for C token diffs"
-    raise Exception('\n'.join([error_msg_header, msg, fix_suggestion]))
-
-lex = clex.CLexer(raise_lex_error, noop, noop, lambda x: True)
-lex.build()
-
 class Retry():
     def __init__(self, retry_times, retry_interval):
         self.retry_times = retry_times
@@ -32,15 +22,35 @@ class Retry():
 
 class Diff():
     class LineDiff():
-        class TokenDiffType(Enum):
-            PLAINTEXT = 0
-            C = 1
+        class LanguageSpecificDiff():
+            class Language(Enum):
+                NOT_SUPPORTED = -1
+                C = 0
+                CPP = 1
+                CSHARP = 2
+                JAVA = 3
+            
+            lexer_to_language = {
+                CLexer : Language.C,
+                CppLexer : Language.CPP,
+                CSharpLexer : Language.CSHARP,
+                JavaLexer : Language.JAVA
+            }
 
-        def __init__(self, patch_line, file_line="", is_missing=True, diff_tokens={}, match_ratio=-1):
+            def __init__(self, language = Language.NOT_SUPPORTED, patch_tokens = [], file_tokens=[], diff_tokens=[]):
+                self.language = language
+                self.patch_tokens = patch_tokens
+                self.file_tokens = file_tokens
+                self.diff_tokens = diff_tokens
+
+        def __init__(self, patch_line, file_line="", is_missing=True, plaintext_diff = [], 
+            language_specific_diff = LanguageSpecificDiff(), match_ratio=-1):
+
             self.patch_line = patch_line
             self.file_line = file_line
             self.is_missing = is_missing
-            self.diff_tokens = diff_tokens
+            self.plaintext_diff = plaintext_diff
+            self.language_specific_diff = language_specific_diff
             self.match_ratio = match_ratio
 
     class MatchStatus(Enum):
@@ -58,40 +68,36 @@ class Diff():
 See docs for output format:
 https://github.com/google/diff-match-patch/wiki/API
 """
-def plaintext_token_diff(patch_line, file_line):
+def calculate_plaintext_diff(patch_line, file_line):
     diff_tokens = dmp.diff_main(patch_line, file_line)
     dmp.diff_cleanupSemantic(diff_tokens)
     return diff_tokens
 
-"""
-Returns a tuple (patch_tokens, file_tokens, diff_tokens) where:
-- patch_tokens are tokens in the patch (in order)
-- file_tokens are tokens in the file (in order)
-- diff_tokens are tokens in the patch but not in the file (not ordered)
-"""
-def c_token_diff(patch_line, file_line):
+def calculate_language_diff(patch_line, file_line, file_name):
+    lexer = get_lexer_for_filename(file_name)
+    language = Diff.LineDiff.LanguageSpecificDiff.lexer_to_language[type(lexer)]
+
+    if language == Diff.LineDiff.LanguageSpecificDiff.Language.NOT_SUPPORTED:
+        return Diff.LineDiff.LanguageSpecificDiff()
+
     patch_tokens = []
-    lex.input(patch_line)
-    cur_token = lex.token()
-    while cur_token: 
-        patch_tokens.append((cur_token.type, cur_token.value))
-        cur_token = lex.token()
-       
+    token_stream = lexer.get_tokens(patch_line)
+    for token in token_stream:
+        patch_tokens.append(token)
+
     file_tokens = []
-    lex.input(file_line)
-    cur_token = lex.token()
-    while cur_token:
-        file_tokens.append((cur_token.type, cur_token.value))
-        cur_token = lex.token()
+    token_stream = lexer.get_tokens(file_line)
+    for token in token_stream:
+       file_tokens.append(token)
     
     diff_tokens = list(set(patch_tokens) - set(file_tokens))
 
-    return (patch_tokens, file_tokens, diff_tokens)
-
-token_type_to_func = {
-    Diff.LineDiff.TokenDiffType.PLAINTEXT : plaintext_token_diff,
-    Diff.LineDiff.TokenDiffType.C : c_token_diff
-}
+    return Diff.LineDiff.LanguageSpecificDiff(
+        language=language,
+        patch_tokens=patch_tokens,
+        file_tokens=file_tokens,
+        diff_tokens=diff_tokens
+    )
 
 # Returns line number of match location, returns -1 if no match
 def fuzzy_search(search_lines, file_name, patch_line_number, retry_obj=None):
@@ -162,8 +168,7 @@ def get_file_without_patch_patch(patch_lines):
     return search_lines
 
 # Returns an object containing information about the difference between a file and a patch
-def find_diffs(patch_lines, file_name, try_already_applied = False, 
-                retry_obj=None, token_diff_types = [Diff.LineDiff.TokenDiffType.PLAINTEXT]):
+def find_diffs(patch_lines, file_name, try_already_applied = False, retry_obj=None):
     # TODO: Get this automatically
     line_number = 36 
     
@@ -208,23 +213,20 @@ def find_diffs(patch_lines, file_name, try_already_applied = False,
         elif max_ratio > LEVENSHTEIN_RATIO:
             matched_file_lines.add(max_ratio_file_line) 
 
-            diff_tokens = {}
-            
-            for token_type in token_diff_types:
-                diff_tokens[token_type] = token_type_to_func[token_type](
-                    patch_line = stripped_patch_line, file_line = max_ratio_file_line)
+            plaintext_diff = calculate_plaintext_diff(stripped_patch_line, max_ratio_file_line)
+
+            language_specific_diff = calculate_language_diff(stripped_patch_line, max_ratio_file_line, file_name)
 
             line_diff_obj = Diff.LineDiff(
                 patch_line = stripped_patch_line,
                 file_line = max_ratio_file_line,
                 is_missing = False,
-                diff_tokens = diff_tokens,
+                plaintext_diff = plaintext_diff,
+                language_specific_diff=language_specific_diff,
                 match_ratio = max_ratio,
             )
             patch_line_type_to_list[patch_line[0]].append(line_diff_obj)
         else:
-            print(stripped_patch_line)
-            print(file_lines)
             missing_diff = Diff.LineDiff(stripped_patch_line)
             patch_line_type_to_list[patch_line[0]].append(missing_diff)
 
@@ -245,13 +247,11 @@ def find_diffs(patch_lines, file_name, try_already_applied = False,
 # patch_file = parse.PatchFile("../patches/CVE-2014-8172.patch")
 # patch_file.getPatch()
 # diff_obj = find_diffs(patch_file.patches[0]._lines, "../../msm-3.10/fs/file_table.c", 
-#     try_already_applied=True, retry_obj=Retry(2,100), 
-#     token_diff_types=[Diff.LineDiff.TokenDiffType.PLAINTEXT, Diff.LineDiff.TokenDiffType.C])
+#     try_already_applied=True, retry_obj=Retry(2,100))
 # print(diff_obj.match_status)
 # print(diff_obj.removed_diffs)
 # print(diff_obj.added_diffs)
 # print(diff_obj.context_diffs)
 # print(diff_obj.additional_lines)
-# for obj in diff_obj.context_diffs:
-#     print(obj.patch_line)
-#     print(obj.diff_tokens[Diff.LineDiff.TokenDiffType.C])
+# for x in diff_obj.context_diffs:
+#     print(x.language_specific_diff.diff_tokens)
