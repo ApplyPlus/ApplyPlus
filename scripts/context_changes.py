6f2a7f69aca8fc2f4281ea5cb834ae73c17e0800
Shayon Banerjee <macbookpro@Shayons-MacBook-Pro.local>
2020-07-30 01:24:59 -0400

    code cleanup

--- scripts/context_changes.py
+++ scripts/context_changes.py
@@ -2,151 +2,133 @@ import patchParser as parse
 import test_match as match
 import slice_and_parse as slice
 import diff_match_patch as dmp_module
+import re
+from enum import Enum
 
-# Tested Patch Files
-# patch_file_name = "../patches/CVE-2016-6828.patch"
-# patch_file_name = "../patches/CVE-2016-4482.patch"
-# patch_file_name = "../patches/CVE-2015-8964.patch"
-# patch_file_name = "../patches/CVE-2016-2550.patch"
-# patch_file_name = "../patches/CVE-2016-3672.patch"
-# patch_file_name = "../patches/CVE-2016-4565.patch"
-# patch_file_name = "../patches/CVE-2014-8172.patch"
-# patch_file_name = "../patches/CVE-2013-7281.patch"
-# patch_file_name = "../patches/CVE-2013-7348.patch"
-# patch_file_name = "../patches/CVE-2014-7822.patch"
-# patch_file_name = "../patches/CVE-2014-8172.patch"
-# patch_file_name = "../patches/CVE-2014-8481.patch"
-# patch_file_name = "../patches/CVE-2014-9090.patch"
-# patch_file_name = "../patches/CVE-2014-9322.patch"
-# patch_file_name = "../patches/CVE-2014-9644.patch"
-# patch_file_name = "../patches/CVE-2014-9710.patch"
-# patch_file_name = "../patches/CVE-2015-0274.patch"
-# patch_file_name = "../patches/CVE-2015-0275.patch"
-# patch_file_name = "../patches/CVE-2015-1333.patch"
-# patch_file_name = "../patches/CVE-2015-1805.patch"
-# patch_file_name = "../patches/CVE-2015-2672.patch"
-# patch_file_name = "../patches/CVE-2015-2925.patch"
-# patch_file_name = "../patches/CVE-2015-3212.patch"
-# patch_file_name = "../patches/CVE-2015-4001.patch"
-# patch_file_name = "../patches/CVE-2015-4176.patch"
-# patch_file_name = "../patches/CVE-2015-4692.patch"
-# patch_file_name = "../patches/CVE-2015-4700.patch"
-# patch_file_name = "../patches/CVE-2015-5697.patch"
-# patch_file_name = "../patches/CVE-2015-6937.patch"
-# patch_file_name = "../patches/CVE-2015-7513.patch"
-# patch_file_name = "../patches/CVE-2015-7613.patch"
-# patch_file_name = "../patches/CVE-2015-7990.patch"
-# patch_file_name = "../patches/CVE-2015-8104.patch"
-# patch_file_name = "../patches/CVE-2015-8215.patch"
-# patch_file_name = "../patches/CVE-2015-8543.patch"
-# patch_file_name = "../patches/CVE-2015-8575.patch"
-# patch_file_name = "../patches/CVE-2015-8816.patch"
-# patch_file_name = "../patches/CVE-2015-8845.patch"
-# patch_file_name = "../patches/CVE-2016-0774.patch"
-# patch_file_name = "../patches/CVE-2016-0823.patch"
-# patch_file_name = "../patches/CVE-2016-1237.patch"
-# patch_file_name = "../patches/CVE-2016-2061.patch"
-# patch_file_name = "../patches/CVE-2016-2069.patch"
-# patch_file_name = "../patches/CVE-2016-2143.patch"
-# patch_file_name = "../patches/CVE-2016-2441.patch"
-# patch_file_name = "../patches/CVE-2016-2442.patch"
-# patch_file_name = "../patches/CVE-2016-2465.patch"
-# patch_file_name = "../patches/CVE-2016-2489.patch"
-# patch_file_name = "../patches/CVE-2016-2550.patch"
-# patch_file_name = "../patches/CVE-2016-3139.patch"
-# patch_file_name = "../patches/CVE-2016-3672.patch"
-# patch_file_name = "../patches/CVE-2016-4482.patch"
-# patch_file_name = "../patches/CVE-2016-4482.patch"
-# patch_file_name = "../patches/CVE-2016-4565.patch"
-# patch_file_name = "../patches/CVE-2016-7117.patch"
-# patch_file_name = "../patches/CVE-2016-7042.patch"
-# patch_file_name = "../patches/CVE-2016-6787.patch"
-# patch_file_name = "../patches/CVE-2016-6327.patch"
-# patch_file_name = "../patches/CVE-2016-4470.patch"
-# patch_file_name = "../patches/CVE-2016-4440.patch"
-# patch_file_name = "../patches/CVE-2016-3070.patch"
-# patch_file_name = "../patches/CVE-2015-8966.patch"
-# patch_file_name = "../patches/CVE-2015-8964.patch"
-# patch_file_name = "../patches/CVE-2015-8963.patch"
-# patch_file_name = "../patches/CVE-2015-8962.patch"
-# patch_file_name = "../patches/CVE-2015-8961.patch"
-# patch_file_name = "../patches/CVE-2015-8952.patch"
+class ContextResult:
+    """
+    Context_Result stores the fields that determine
+        whether the patch needs to be applied or not
 
-# GOLDEN
-# patch_file_name = "../patches/CVE-2015-0572.patch"
+    run_patch: Boolean that determines if the patch 
+        should still be applied
 
-# DEFINE STATEMENTS ...
-# patch_file_name = "../patches/CVE-2014-9803.patch"
+    message: Is a string that describes the output 
+        of the context related changes 
+    """
 
+    def __init__(self, status, messages):
+        self.status = status
+        self.messages = messages
 
-def context_changes(patch_file_name):
-    patch_file = parse.PatchFile(patch_file_name)
+class CONTEXT_DECISION(Enum):
+    DONT_RUN = 0
+    RUN = 1
+
+def context_changes(patch_file_path):
+    """
+    context_changes(str): takes in a patch file path and 
+        returns a ContextResult object that determines 
+        weather to run the patch or not and an associate 
+        message that can be shown to users
+
+    patch_file_path: string representing the path to a patch file
+    """
+    context_result_list = []
+
+    patch_file = parse.PatchFile(patch_file_path)
     patch_file.getPatch()
 
-    file_name = patch_file.patches[0].getFileName()
-    file_name = f"../../../msm-3.10{file_name}"
+    for patch in patch_file.patches:
+        file_name = patch.getFileName()
+        # TODO: update this to not be hardcoded
+        file_path = f"../../../msm-3.10{file_name}"
 
-    sp = slice.SliceParser(file_name)
-    file_slice = sp.slice_parse()
+        file_slice = slice.SliceParser(file_path)
+        file_slice_parsed = file_slice.slice_parse()
 
-    diff_obj = match.find_diffs(
-        patch_file.patches[0],
-        file_name,
-        try_already_applied=True,
-        retry_obj=match.Retry(2, 100),
-    )
+        # TODO: find optimal retry object 
+        # (maybe do 3 runs while expanding scope)
+        diff_file_patch = match.find_diffs(
+            patch,
+            file_path,
+            try_already_applied=True,
+            retry_obj=match.Retry(3, 100),
+        )
 
-    if diff_obj.match_status == match.Diff.MatchStatus.MATCH_FOUND:
-        if diff_obj.context_diffs:
-            for context_diffs in diff_obj.context_diffs:
-                if context_diffs.match_ratio > match.LEVENSHTEIN_RATIO:
-                    function_name = context_diffs.function_for_patch
+        if diff_file_patch.match_status != match.Diff.MatchStatus.MATCH_FOUND:
+            context_result = ContextResult(CONTEXT_DECISION.DONT_RUN.value, 
+                [f"A context match was not found."])
+            context_result_list.append([context_result])
+            continue
+        
+        if not diff_file_patch.context_diffs:
+            context_result = ContextResult(CONTEXT_DECISION.DONT_RUN.value, 
+                [f"No context related issues found."])
+            context_result_list.append([context_result])
+            continue
+            
+        context_diffs_list = []
+        for context_diff in diff_file_patch.context_diffs:
+            if context_diff.match_ratio < match.LEVENSHTEIN_RATIO:
+                output_message = ("The match ratio between the source and patch code was low. "
+                    f"We received a match ratio of {context_diff.match_ratio}, expected a ratio" 
+                    f" greater than or equal to {match.LEVENSHTEIN_RATIO}.")
+            
+                context_result = ContextResult(CONTEXT_DECISION.DONT_RUN.value, [output_message])
+                context_diffs_list.append(context_result)
+                continue
+
+            if re.search('^(\w+( )?){2,}\([^!@#$+%^]+?\)', context_diff.file_line):
+                output_message = (f"We noticed a context-related difference between the function"
+                    f" in the source code [{context_diff.file_line}] We are suggesting to replace"
+                    f" this function with [{context_diff.patch_line}]")
+                # TODO: should we give information from the slicer here? 
+                # TODO: should this be a false?
+                context_result = ContextResult(CONTEXT_DECISION.DONT_RUN.value, [output_message])
+                context_diffs_list.append(context_result)
+                continue
+                
+            diff_match = dmp_module.diff_match_patch()
+            line_patch_diffs = diff_match.diff_main(
+                    context_diff.file_line, context_diff.patch_line
+            )
+
+            if not line_patch_diffs:
+                context_result = ContextResult(CONTEXT_DECISION.RUN.value, 
+                    ["We have found a match, run the patch."])
+                context_diffs_list.append(context_result)
+                continue
+            
+            print(line_patch_diffs)
+            function_name = context_diff.function_for_patch
+            output_messages = []
+            for line_diff in line_patch_diffs:
+                if line_diff[0] == 0 and line_diff[1].rstrip().endswith("="):
+                    output_messages.append("An R-Value has been changed, the slicer does not have" 
+                        " information about R-Value variables.")
+                
+                elif line_diff[0] == 0 and line_diff[1].rstrip().startswith("="):
                     if function_name:
                         function_name = function_name.split("(")[0].split()[-1]
+                        file_var = [
+                            item for item in line_patch_diffs if item[0] == -1
+                        ][0]
 
-                        if "(" in context_diffs.file_line and ")" in context_diffs.file_line:
-                            # handle function context diff
-                            print(
-                                f"We noticed a context-related difference between the function in the source code [{context_diffs.file_line}]"
-                            )
-                            print(
-                                f"We are suggesting to replace this function with [{context_diffs.patch_line}]"
-                            )
-                            break
-
-                        diff_match = dmp_module.diff_match_patch()
-                        line_patch_diffs = diff_match.diff_main(
-                            context_diffs.file_line, context_diffs.patch_line
-                        )
-
-                        print(line_patch_diffs)
-                        # handles R-Value Variables
-                        for diff in line_patch_diffs:
-                            if diff[0] == 0 and diff[1].rstrip().endswith("="):
-                                print(
-                                    "[SLICER_MESSAGE]: An R-Value has been changed, the slicer does not have information about R-Value variables."
-                                )
-                                break
-
-                        # handle l-value variable context conflict
-                        for diff in line_patch_diffs:
-                            if diff[0] == 0 and diff[1].rstrip().startswith("="):
-                                # retrive L-Value variable being changed
-                                file_var = [
-                                    item for item in line_patch_diffs if item[0] == -1
-                                ][0]
-
-                                var_statistics = file_slice[function_name][file_var]
-                                print(var_statistics)
+                        var_information = file_slice_parsed[function_name][file_var]
+                        output_messages.append(f"L-Value change, User should consider the following"
+                            f" slicer_output: {var_information})")
                     else:
-                        print("WARNING: Function was not supplied. Skipping this patch.")
+                        output_messages.append("L-Value change but function name was not supplied by"
+                            " matching code, we can not provide slicer information")
 
-                else:
-                    print("The match ratio between the source and patch code was low")
-                    print(
-                        f"We received a match ratio of {context_diffs.match_ratio}, expected a ratio greater than {match.LEVENSHTEIN_RATIO}"
-                    )
-        else:
-            print("[SLICER_WARNING]: No context related issues found")
-    else:
-        print("[SLICER_WARNING]: A context match was not found.")
+            if not output_messages:
+                output_messages.append("Context difference was neither an L-Value or R-Value change.")
+            
+            context_result = ContextResult(CONTEXT_DECISION.RUN.value, output_messages)
+            context_diffs_list.append(context_result)
+        
+        context_result_list.append(context_diffs_list)
+        
+    return context_result_list
+    
