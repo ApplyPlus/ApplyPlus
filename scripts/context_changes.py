a6d8d0bf3f617fcbb79e14f5a7a95b92dbf58408
Shayon Banerjee <macbookpro@Shayons-MacBook-Pro.local>
2020-08-06 14:45:02 -0400

    context_changes update + unit tests

--- scripts/context_changes.py
+++ scripts/context_changes.py
@@ -46,7 +46,7 @@ def context_changes(sub_patch):
     # TODO: find optimal retry object
     # (maybe do 3 runs while expanding scope)
     diff_file_patch = match.find_diffs(
-        patch,
+        sub_patch,
         file_path,
         try_already_applied=True,
         retry_obj=match.Retry(3, 100),
@@ -54,38 +54,38 @@ def context_changes(sub_patch):
 
     if diff_file_patch.match_status != match.Diff.MatchStatus.MATCH_FOUND:
         context_result = ContextResult(CONTEXT_DECISION.DONT_RUN.value, 
-            [f"A context match was not found."])
+            "A context match was not found.")
         
         return context_result
         
     
     if not diff_file_patch.context_diffs:
-        context_result = ContextResult(CONTEXT_DECISION.DONT_RUN.value, 
-            [f"No context related issues found."])
+        context_result = ContextResult(CONTEXT_DECISION.RUN.value, 
+            "No context related issues found.")
         
         return context_result
         
     apply_patch = True
     output_message = ""
     context_diff_count = 1
+    
     for context_diff in diff_file_patch.context_diffs:
         if context_diff.match_ratio < match.LEVENSHTEIN_RATIO:
-            output_message += (f"Context Diff #{context_diff_count}: The match ratio between the source and patch code was low. "
-                    f"We received a match ratio of {context_diff.match_ratio}, expected a ratio" 
-                    f" greater than or equal to {match.LEVENSHTEIN_RATIO}. The patch line was {context_diff.patch_line}"
-                    f" whereas the file line was {context_diff.file_line}\n")
+            output_message += (f"Context Diff #{context_diff_count}: The match ratio between the source and patch code was low. We received a match ratio of {context_diff.match_ratio}, expected a ratio greater than or equal to {match.LEVENSHTEIN_RATIO}. The patch line was {context_diff.patch_line} whereas the file line was {context_diff.file_line}\n")
                 
             apply_patch &= CONTEXT_DECISION.DONT_RUN.value
+            context_diff_count += 1
+            continue
 
         elif re.search('^(\w+( )?){2,}\([^!@#$+%^]+?\)', context_diff.file_line):
-            output_message += (f"Context Diff #{context_diff_count}: We noticed a context-related difference between the function"
-                    f" in the source code [{context_diff.file_line}] We are suggesting to replace"
-                    f" this function with [{context_diff.patch_line}]\n")
+            output_message += (f"Context Diff #{context_diff_count}: We noticed a context-related difference between the function in the source code [{context_diff.file_line}] We are suggesting to replace this function with [{context_diff.patch_line}]\n")
             
             # TODO: should we give information from the slicer here? 
             # TODO: should this be a false?
             
             apply_patch &= CONTEXT_DECISION.DONT_RUN.value
+            context_diff_count += 1
+            continue
         
         diff_match = dmp_module.diff_match_patch()
         line_patch_diffs = diff_match.diff_main(
@@ -112,24 +112,19 @@ def context_changes(sub_patch):
                     ][0]
 
                     var_information = file_slice_parsed[function_name][file_var]
-                    output_message += f"Context Diff #{context_diff_count}: R-Value change, User should consider the following\n"
-                            f" slicer_output: {var_information})")
+                    output_message += f"Context Diff #{context_diff_count}: R-Value change, User should consider the following slicer_output: {var_information}"
                     apply_patch &= CONTEXT_DECISION.RUN.value
+
                     
                 else:
-                    output_message += (f"Context Diff #{context_diff_count}: R-Value change but function name was not supplied by"
-                            " matching code, we can not provide slicer information\n")
+                    output_message += (f"Context Diff #{context_diff_count}: R-Value change but function name was not supplied by matching code, we can not provide slicer information\n")
                     apply_patch &= CONTEXT_DECISION.RUN.value
 
                 break
-            
-            else:
-                # TODO: verify this is the correct
-                apply_patch &= CONTEXT_DECISION.RUN.value
         
         context_diff_count += 1
         
-    return context_result = ContextResult(apply_patch, output_message)
+    return ContextResult(apply_patch, output_message)
 
                     
 
--- scripts/context_changes_unittest.py
+++ scripts/context_changes_unittest.py
@@ -2,14 +2,103 @@ import unittest
 import context_changes as context
 import patchParser as parse
 
-class TestContextChanges(unittest.TestCase:)
+class TestContextChanges(unittest.TestCase):
 
-def test_subpatch(self) :
-    patch_file = parse.PatchFile("../vulnerableforks/patches/CVE-2014-8172.patch")
-    patch_file.getPatch()
-    result = context.context_changes(patch_file.patches[0])
-    self.assertTrue(result.status)
+    def test_patch_CVE_2014_8172(self):
+        patch_file = parse.PatchFile("../patches/CVE-2014-8172.patch")
+        patch_file.getPatch()
+        
+        result = context.context_changes(patch_file.patches[0])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[1])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[2])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[3])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[4])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[5])
+        self.assertTrue(result.status)
+        
+        result = context.context_changes(patch_file.patches[6])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[7])
+        self.assertTrue(result.status)
+        
+        result = context.context_changes(patch_file.patches[8])
+        self.assertFalse(result.status)
+
+        # VERY GOOD FOR THE DEMO
+        result = context.context_changes(patch_file.patches[9])
+        print(result.messages)
+        self.assertFalse(result.status)
+
+        result = context.context_changes(patch_file.patches[10])
+        self.assertFalse(result.status)
+
+    def test_patch_CVE_2015_0572(self):
+        patch_file = parse.PatchFile("../patches/CVE-2015-0572.patch")
+        patch_file.getPatch()
+
+        # TODO: check this patch, odd behaviour
+        result = context.context_changes(patch_file.patches[0])
+        self.assertFalse(result.status)
+
+        result = context.context_changes(patch_file.patches[1])
+        self.assertFalse(result.status)
+
+        result = context.context_changes(patch_file.patches[2])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[3])
+        print(result.messages)
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[4])
+        self.assertFalse(result.status)
+
+        result = context.context_changes(patch_file.patches[5])
+        self.assertTrue(result.status)
+        
+        result = context.context_changes(patch_file.patches[6])
+        self.assertTrue(result.status)
+    
+        result = context.context_changes(patch_file.patches[7])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[8])
+        self.assertTrue(result.status)
 
 
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
 if __name__ == '__main__':
     unittest.main()
