b1f20939c7f37cb10ccb86262d9bdb87f50e0178
Yuvika K <kyuvika2000@gmail.com>
2020-09-09 13:51:45 -0400

    Added unittests

--- tests/test_context_changes.py
+++ tests/test_context_changes.py
@@ -123,13 +123,6 @@ class TestContextChanges(unittest.TestCase):
 
         result = context.context_changes(patch_file.patches[0])
         self.assertFalse(result.status)
-    
-    def test_patch_CVE_2015_8845(self):
-        patch_file = parse.PatchFile("/Users/yuvika/Desktop/vulnerable_patches/vulnerableforks/patches/CVE-2015-8845.patch")
-        patch_file.getPatch()
-
-        result = context.context_changes(patch_file.patches[0])
-        self.assertFalse(result.status)
 
     def test_patch_CVE_2013_7281(self):
         patch_file = parse.PatchFile("/Users/yuvika/Desktop/vulnerable_patches/vulnerableforks/patches/CVE-2013-7281.patch")
@@ -236,6 +229,154 @@ class TestContextChanges(unittest.TestCase):
         result = context.context_changes(patch_file.patches[5])
         self.assertTrue(result.status)
 
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[6])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[7])
+        self.assertTrue(result.status)
+
+    def test_patch_CVE_2014_9644(self):
+        patch_file = parse.PatchFile("/Users/yuvika/Desktop/URA/vulnerableforks/patches/CVE-2014-9644.patch")
+        patch_file.getPatch()
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[0])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[1])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[2])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[3])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[4])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[5])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[5])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[6])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[7])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[8])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[9])
+        self.assertTrue(result.status)
+
+        # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[10])
+        self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[11])
+        self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[12])
+        self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[13])
+        self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[14])
+        self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[15])
+        self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[16])
+        self.assertTrue(result.status)
+
+       # This file has been deleted.
+       # result = context.context_changes(patch_file.patches[17])
+       # self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[18])
+        self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[19])
+        self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[20])
+        self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[21])
+        self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[22])
+        self.assertTrue(result.status)
+
+       # Subpatch has been applied
+        result = context.context_changes(patch_file.patches[23])
+        self.assertTrue(result.status)
+
+    def test_patch_CVE_2015_3212(self):
+        # patch_file = parse.PatchFile("../vulnerableforks/patches/CVE-2014-8481.patch")
+        patch_file = parse.PatchFile("/Users/yuvika/Desktop/URA/vulnerableforks/patches/CVE-2015-3212.patch")
+        patch_file.getPatch()
+
+        result = context.context_changes(patch_file.patches[0])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[1])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[2])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[3])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[4])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[5])
+        self.assertTrue(result.status)
+
+        # Look into why this is failing! 
+        result = context.context_changes(patch_file.patches[6])
+        print(result.messages)
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[7])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[8])
+        self.assertTrue(result.status)
+
+        result = context.context_changes(patch_file.patches[9])
+        self.assertTrue(result.status)
 
 if __name__ == "__main__":
     unittest.main()
