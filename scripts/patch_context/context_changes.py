09b01871b9b5a1eaf05c542d32933dbdb6038df4
Shayon Banerjee <macbookpro@Shayons-MacBook-Pro-2.local>
2020-09-17 22:46:30 -0400

    refactor context_changes

--- scripts/patch_context/context_changes.py
+++ scripts/patch_context/context_changes.py
@@ -95,68 +95,171 @@ def context_changes(sub_patch, expand=False):
     output_message = ""
 
     for context_diff in diff_file_patch.context_diffs:
+
         if context_diff.match_ratio < match.LEVENSHTEIN_RATIO:
-            output_message += (
-                f"The match ratio between the source and patch code was low. "
-                f"We received a match ratio of {context_diff.match_ratio}, expected a ratio greater than or equal to {match.LEVENSHTEIN_RATIO}. "
-                f"The patch line was {context_diff.patch_line} whereas the file line was {context_diff.file_line}\n"
+            output_message = (
+                f"For the context line difference in the patch file {context_diff.patch_line}"
+                f" and in the source file {context_diff.file_line} the match ratio {context_diff.match_ratio}"
+                f" is below the threashold match ratio of {match.LEVENSHTEIN_RATIO}. For this reason, we recommend"
+                f" to not run this patch."
             )
 
-            apply_patch &= CONTEXT_DECISION.DONT_RUN.value
-            continue
-
-        elif re.search("(\w+( )?){2,}\([^!@#$+%^]+?\)", context_diff.file_line):
-            output_message += (
-                f"We noticed a context-related difference between the "
-                f"function in the source code [{context_diff.file_line}] We are suggesting to replace this function with [{context_diff.patch_line}]\n"
+            context_result = ContextResult(
+                CONTEXT_DECISION.DONT_RUN.value,
+                output_message,
+                diff_file_patch,
+                False,
             )
 
-            # TODO: should we give information from the slicer here?
-            # TODO: should this be a false?
+            return context_result
 
-            apply_patch &= CONTEXT_DECISION.DONT_RUN.value
+        rhs_function_call_reg = re.search(
+            "=( ?)(\w+( )?){2,}\([^!@#$+%^]+?\)", context_diff.file_line
+        )
+        lhs_function_call_reg = re.search(
+            "( +)(\w+( )?)\([^!@#$+%^]+?\)", context_diff.file_line
+        )
+        function_definition_reg = re.search(
+            "\w+( )?){2,}\([^!@#$+%^]+?\)", context_diff.file_line
+        )
+
+        if rhs_function_call_reg:
+            output_message = (
+                f"For the context line difference in the patch file {context_diff.patch_line}"
+                f" and in the source file {context_diff.file_line} represents a function call on the"
+                f" RHS of an expression. Since this the value on the LHS of the expression may have "
+                f" dependencies at other locations in the file, we recommend to not run this patch."
+            )
+
+            context_result = ContextResult(
+                CONTEXT_DECISION.DONT_RUN.value,
+                output_message,
+                diff_file_patch,
+                False,
+            )
+
+            return context_result
+
+        # A function call is being made while not being assigned to a variable
+        # we will continue with running the patch for this case
+        elif lhs_function_call_reg.group() == context_diff.file_line:
             continue
+
+        elif function_definition_reg:
+            output_message = (
+                f"For the context line difference in the patch file {context_diff.patch_line}"
+                f" and in the source file {context_diff.file_line} represents a function definition."
+                f" Since the previous function defintion in the source file may have dependencies at "
+                f" other areas in the code base, we recommend to not run this patch."
+            )
+
+            context_result = ContextResult(
+                CONTEXT_DECISION.DONT_RUN.value, output_message, diff_file_patch, False
+            )
+
+            return context_result
 
         diff_match = dmp_module.diff_match_patch()
         line_patch_diffs = diff_match.diff_main(
             context_diff.file_line, context_diff.patch_line
         )
 
-        # dissect the context line to determine type
-        function_name = context_diff.function_for_patch
-        for line_diff in line_patch_diffs:
-            if line_diff[0] == 0 and line_diff[1].rstrip().endswith("="):
-                output_message += (
-                    f"An L-Value has been changed, we will not apply the patch.\n"
-                )
-                apply_patch &= CONTEXT_DECISION.DONT_RUN.value
-                break
+        unchanged_diff_list = [
+            item[1] for item in line_patch_diffs if item[0] == 0 and "=" in item[1]
+        ]
 
-            elif line_diff[0] == 0 and line_diff[1].rstrip().startswith("="):
+        if not unchanged_diff_list:
+            output_message = (
+                f"For the context line difference in the patch file {context_diff.patch_line}"
+                f" and in the source file {context_diff.file_line} represents neither a function"
+                f"definition, function call, L-Value, or R-Value change."
+                f" For this reason we recommend to not run this patch. "
+            )
+
+            context_result = ContextResult(
+                CONTEXT_DECISION.DONT_RUN.value, output_message, diff_file_patch, False
+            )
+
+            return context_result
+
+        else:
+            unchanged_diff = unchanged_diff_list[0]
+
+            if unchanged_diff.rstrip().startswith("="):
+                output_message = (
+                    f"For the context line difference in the patch file {context_diff.patch_line}"
+                    f" and in the source file {context_diff.file_line} represents an L-Value change."
+                    f" For this reason we recommend to not run this patch. "
+                )
+
+                context_result = ContextResult(
+                    CONTEXT_DECISION.DONT_RUN.value,
+                    output_message,
+                    diff_file_patch,
+                    False,
+                )
+
+                return context_result
+
+            elif unchanged_diff.rstrip().endswith("="):
+                # dissect the context line to determine type
+                function_name = context_diff.function_for_patch
+                potential_file_var = [
+                    item for item in line_patch_diffs if item[0] == -1
+                ][0]
+
                 if function_name:
                     function_name = function_name.split("(")[0].split()[-1]
-                    file_var = [item for item in line_patch_diffs if item[0] == -1][0]
+                    try:
+                        var_information = file_slice_parsed[function_name][file_var]
+                    except:
+                        var_information = None
 
-                    var_information = file_slice_parsed[function_name][file_var]
-                    output_message += f"R-Value change, User should consider the following slicer_output: {var_information}"
-                    apply_patch &= CONTEXT_DECISION.RUN.value
+                    # L-Value change on the RHS of expression
+                    if var_information:
+                        output_message = (
+                            f"For the context line difference in the patch file {context_diff.patch_line}"
+                            f" and in the source file {context_diff.file_line} represents an L-Value change."
+                            f" For this reason we recommend to not run this patch. "
+                        )
 
+                        context_result = ContextResult(
+                            CONTEXT_DECISION.DONT_RUN.value,
+                            output_message,
+                            diff_file_patch,
+                            False,
+                        )
+
+                        return context_result
+                    else:
+                        # R-Value change we continue with running the patch
+                        continue
                 else:
-                    output_message += (
-                        f"R-Value change but function name was not "
-                        f"supplied by matching code, we can not provide slicer information\n"
-                    )
-                    apply_patch &= CONTEXT_DECISION.RUN.value
+                    # we don't know the function name
+                    is_var = False
+                    for _, value in file_slice_parsed.items():
+                        for var_name, _ in value.items():
+                            if var_name == potential_file_var:
+                                is_var = True
 
-                break
-            else:
-                output_message += (
-                    f"The context change was neither an L-Value, R-Value, or function name change; "
-                    "however, the match percentage was above the Leveinstein Ratio at {context_diff.match_ratio}"
-                )
-                apply_patch &= CONTEXT_DECISION.DONT_RUN.value
+                    if is_var:
+                        output_message = (
+                            f"For the context line difference in the patch file {context_diff.patch_line}"
+                            f" and in the source file {context_diff.file_line} represents an L-Value change."
+                            f" For this reason we recommend to not run this patch. "
+                        )
 
-                break
+                        context_result = ContextResult(
+                            CONTEXT_DECISION.DONT_RUN.value,
+                            output_message,
+                            diff_file_patch,
+                            False,
+                        )
+
+                        return context_result
+                    else:
+                        # R-Value
+                        continue
 
     if diff_file_patch.additional_lines:
         for add_lines in diff_file_patch.additional_lines:
@@ -175,4 +278,6 @@ def context_changes(sub_patch, expand=False):
     else:
         comment_line = False
 
-    return ContextResult(apply_patch, output_message, diff_file_patch, comment_line)
+    return ContextResult(
+        True, "This Patch can be applied.", diff_file_patch, comment_line
+    )
